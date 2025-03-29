f73aff4fb028627940b754ce57390971192f06ec
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2025-03-28 23:43:25 -0600

    chore: remove unused files and clean up project structure

--- Unit 04/Pathway 02/basic-android-kotlin-compose-training-cupcake/app/src/androidTest/java/com/example/cupcake/test/ScreenAssertions.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cupcake.test
-
-import androidx.navigation.NavController
-import org.junit.Assert.assertEquals
-
-fun NavController.assertCurrentRouteName(expectedRouteName: String) {
-    assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
-}
